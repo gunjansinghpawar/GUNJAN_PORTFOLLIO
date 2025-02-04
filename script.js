0583560169d4714d9f445782b1946c544c447f78
PAWAR GUNJAN SINGH <pawargunjan36@gmail.com>
2025-02-04 18:54:25 +0530

    Update Code

--- script.js
+++ script.js
@@ -26,8 +26,47 @@ function closeMenu() {
   sideMenu.style.right = "-200px";
 }
 
-function redirectToRepository() {
-  window.location.href = 'https://github.com/gunjansinghpawar/Netflix--React'; // Replace with your repository link
+function redirectToNetflixRepository() {
+  window.location.href = './404.html';
 }
 
+function redirectToTravelFrontendRepository(){
+  window.location.href = 'https://github.com/gunjansinghpawar/frontend-djtoursandtravels'; 
+}
 
+function redirectToTravelBackendRepository(){
+  window.location.href = 'https://github.com/gunjansinghpawar/backend-djtoursandtravels'; 
+}
+function redirectToPortfolioRepository(){
+  window.location.href = 'https://github.com/gunjansinghpawar/GUNJAN_PORTFOLLIO';
+}
+
+function redirectToCalcRepository(){
+  window.location.href = 'https://github.com/gunjansinghpawar/Calculator';
+}
+
+function redirectToMusicRepository(){
+  window.location.href = 'https://github.com/gunjansinghpawar/Spotify--refrence';
+}
+
+function redirectToCoffeeRepository(){
+  window.location.href = 'https://github.com/gunjansinghpawar/Coffee';
+}
+
+function redirectToTextRepository(){
+  window.location.href = 'https://github.com/gunjansinghpawar/TextUtils';
+}
+
+function redirectToLibraryRepository(){
+  window.location.href = 'https://github.com/gunjansinghpawar/librarySystem';
+}
+function scrollProjects(direction) {
+  const container = document.querySelector('.project-container');
+  const scrollAmount = 300; // Adjust scroll distance as needed
+
+  if (direction === 'left') {
+    container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+  } else if (direction === 'right') {
+    container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+  }
+}
